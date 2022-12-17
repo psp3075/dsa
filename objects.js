f891d0ad7b7e6d6a98b98fbc216affda98170603
Pavan S Patil <psp3075@gmail.com>
2022-12-17 13:40:27 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,27 @@
+//not iterable
+const obj = {
+  name: 'Adam',
+  age: 25,
+  'test-3': true,
+  printMyName: function() {
+    console.log(this.name)
+  }
+}
+
+obj.hobby = 'football'
+delete obj.hobby
+
+console.log(obj.name)
+console.log(obj['age'])
+console.log(obj['test-3']) //not possible with .notation
+console.log(obj)
+obj.printMyName()
+
+console.log(Object.entries(obj)) // - O(n)
+//Object.values() - O(n)
+//Object.entries() - O(n)
+
+//Insert - O(1)
+//Remove - O(1)
+//Access - O(1)
+//Search - O(n)
